ebfa584137c76f3a8210cf2f5553abda2dd07121
Lev <lev@lev>
2023-06-12 10:45:54 +0300

    3 of 4 stages completed

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -1,20 +1,43 @@
 const input = require('sync-input');
-const currencies = {USD: 1.0, JPY: 113.5, EUR: 0.89, RUB: 74.36, GBP: 0.75};
 
-console.log(`Welcome to Currency Converter!
-1 USD equals ${currencies['USD']} USD
-1 USD equals ${currencies['JPY']} JPY
-1 USD equals ${currencies['EUR']} EUR
-1 USD equals ${currencies['RUB']} RUB
-1 USD equals ${currencies['GBP']} GBP
-I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP
-Type the currency you wish to convert: USD`);
+const currencies = {
+    USD: 1,
+    JPY: 113.5,
+    EUR: 0.89,
+    RUB: 74.36,
+    GBP: 0.75,
+};
 
-let to = input('To:').toUpperCase();
-if (!(to in currencies)) console.log('Unknown currency');
-else {
-    let amount = Number(input('Amount:'));
-    if (amount < 1) console.log('The amount cannot be less than 1');
-    else if (isNaN(amount)) console.log("The amount has to be a number");
-    else console.log(`Result: ${amount} USD equals ${(amount * currencies[to]).toFixed(4)} ${to}`);
+console.log("Welcome to Currency Converter!");
+console.log("1 USD equals 1 USD");
+console.log("1 USD equals 113.5 JPY");
+console.log("1 USD equals 0.89 EUR");
+console.log("1 USD equals 74.36 RUB");
+console.log("1 USD equals 0.75 GBP");
+
+console.log("What do you want to convert?");
+let fromCurrency = input("From: ").toUpperCase();
+
+if (!(fromCurrency in currencies)) {
+    console.log("Unknown currency");
+} else {
+    console.log("To: ");
+    let toCurrency = input("To: ").toUpperCase();
+
+    if (!(toCurrency in currencies)) {
+        console.log("Unknown currency");
+    } else {
+        console.log("Amount: ");
+        let amount = input("Amount: ");
+
+        if (isNaN(Number(amount))) {
+            console.log("The amount has to be a number");
+        } else if (Number(amount) < 1) {
+            console.log("The amount cannot be less than 1");
+        } else {
+            let result = (Number(amount) * currencies[toCurrency]) / currencies[fromCurrency];
+            console.log(`Result: ${amount} ${fromCurrency} equals ${result.toFixed(4)} ${toCurrency}`);
+        }
+    }
 }
+
